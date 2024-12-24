d36bda2e5eb60df0083a6d1ecbe62ad363901f47
Gihzx <145726882+Gihzx@users.noreply.github.com>
2024-12-23 22:04:45 -0300

    fix: ajuste de codigo hitorico de compras

--- src/pages/historico/compras.tsx
+++ src/pages/historico/compras.tsx
@@ -13,9 +13,21 @@ import {
 import { useColorModeValue } from '~components/ui/color-mode'
 import { Header } from '~components/Header'
 import { Title } from '~components/Title'
+import { Compra } from '~components/Compras'
 
 export default function Compras() {
   const bg = useColorModeValue('gray.100', 'gray.800')
+  const imageData = [
+    {
+      src: '/images/cebola.png',
+      alt: 'Ceboa roxa',
+      produto: 'produto A',
+      tipo: 'Natural',
+      peso: '1kg',
+      qtd: '2 kl',
+      data: '20/14/2024',
+    },
+  ]
 
   return (
     <>
@@ -30,12 +42,20 @@ export default function Compras() {
         >
           <GridItem area={'main'}>
             <Flex justifyContent="space-between" h="100vh">
-              <Flex as="form" bg={bg} p="8" borderRadius={8} flexDir="column">
+              <Flex
+                as="form"
+                bg={bg}
+                p="8"
+                borderRadius={8}
+                flexDir="column"
+            
+              >
                 <Flex
                   justifyContent="space-between"
                   w="100%"
                   gap="3"
                   fontWeight="bold"
+                  
                 >
                   <Text>HISTÓRICO DE VENDAS</Text>
                 </Flex>
@@ -44,45 +64,13 @@ export default function Compras() {
                   borderColor="gray.900"
                   borderWidth="1px"
                   w={900}
+
                 />
-                <Flex justifyContent="space-between" alignItems={'center'}>
-                  <Flex justifyContent="space-between" flexDir="column">
-                    <Image
-                      src={'/images/cebola.png'}
-                      alt="logo"
-                      width={'100%'}
-                      objectFit="cover"
-                      boxSize="100%"
-                    ></Image>
-                  </Flex>
-                  <Flex justifyContent="space-between" flexDir="column">
-                    <Text>Cebola Roxa - Verdura</Text>
-                    <Text>Tipo: 100% natural</Text>
-                    <Text>Peso: 1,5Kg </Text>
-                  </Flex>
-                  <Flex flexDir="column" alignItems={'center'}>
-                    <Text>QTD</Text>
-                    <Text>04</Text>
-                  </Flex>
-                  <Flex justifyContent="space-between" flexDir="column">
-                    <Text>Data de compra</Text>
-                    <Text>10/10/2025</Text>
-                  </Flex>
-                  <Flex justifyContent="space-between" flexDir="column">
-                    <Button
-                      type="submit"
-                      mt="5"
-                      bg={useColorModeValue('green.700', 'green.500')}
-                      size="lg"
-                      fontSize={'15px'}
-                      color={useColorModeValue('gray.100', 'gray.100')}
-                    >
-                      COMPRAR NOVAMENTE
-                    </Button>
+                <Flex flexDir={'column'}  justifyContent="space-between" w="100%" >
+                  <Compra images={imageData} />
                   </Flex>
                 </Flex>
               </Flex>
-            </Flex>
           </GridItem>
         </Grid>
       </Box>
